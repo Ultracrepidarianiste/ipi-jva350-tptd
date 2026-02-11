89fe06b0c0e2f6e144c7f032c98e231be0daec57
Yann Bernard <yannbernard61@gmail.com>
2026-02-11 14:52:11 +0100

    premier test divisé en deux tests

--- src/main/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/main/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -4,50 +4,34 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class SalarieAideADomicileTest {
-
+    // Attendu qu'on ait un False
     @Test
-    public void testALegalementDroitADesCongesPayes() {
+    public void testALegalementDroitADesCongesPayesNON() {
         //given
         SalarieAideADomicile s = new SalarieAideADomicile();
 
-        // TEST 1 : 0 jours travaillés
         //When
         s.setJoursTravaillesAnneeNMoins1(0);
         //then
         assertFalse(s.aLegalementDroitADesCongesPayes());
         // assertTrue(s.aLegalementDroitADesCongesPayes());
+    }
+    // Attendu qu'on ait un true
+    @Test
+    public void testALegalementDroitADesCongesPayesOUI() {
+        //given
+        SalarieAideADomicile s = new SalarieAideADomicile();
 
-        // TEST 2 : 10 jours travaillés
-        s.setJoursTravaillesAnneeNMoins1(10);
-        assertFalse(s.aLegalementDroitADesCongesPayes());
-
-        // TEST 3 : 11 jours travaillés Donc celui où il a le droit
+        //When
         s.setJoursTravaillesAnneeNMoins1(11);
+        //then
         assertTrue(s.aLegalementDroitADesCongesPayes());
+        // assertFalse(s.aLegalementDroitADesCongesPayes());
     }
 
     // SalarieAideADomicile.calculeJoursDeCongeDecomptesPourPlage().
 
-    @Test
-    public void testALegalementDroitADesCongesPayes() {
-        //given
-        SalarieAideADomicile s = new SalarieAideADomicile();
 
-        // TEST 1 : 0 jours travaillés
-        //When
-        s.setJoursTravaillesAnneeNMoins1(0);
-        //then
-        assertFalse(s.aLegalementDroitADesCongesPayes());
-        // assertTrue(s.aLegalementDroitADesCongesPayes());
-
-        // TEST 2 : 10 jours travaillés
-        s.setJoursTravaillesAnneeNMoins1(10);
-        assertFalse(s.aLegalementDroitADesCongesPayes());
-
-        // TEST 3 : 11 jours travaillés Donc celui où il a le droit
-        s.setJoursTravaillesAnneeNMoins1(11);
-        assertTrue(s.aLegalementDroitADesCongesPayes());
-    }
 
 
 
